fd46dbfe3d2ed2c37d7efc1f968d2ba4cc341e2f
Hanafi_dev <hanafiahtkj@gmail.com>
2022-12-12 14:54:29 +0800

    #

--- resources/views/landing-page/index.blade.php
+++ resources/views/landing-page/index.blade.php
@@ -66,7 +66,7 @@
     <!-- ===============================================-->
     <main class="main" id="top">
       <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3 d-block" data-navbar-on-scroll="data-navbar-on-scroll">
-        <div class="container"><a class="navbar-brand" href="index.html"> <img class="me-3 d-inline-block" src="{{ asset('images/LOGO2.png') }}" alt="" height="50" /></a>
+        <div class="container"><a class="navbar-brand" href="index.html"> <img class="me-3 d-inline-block" src="{{ asset('images/LOGO2.png') }}" alt="" height="45" /></a>
           <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
           <div class="collapse navbar-collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto pt-2 pt-lg-0 font-base">
